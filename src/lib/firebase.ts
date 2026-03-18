5a218da4bf8d62e8ffe703c17d6edaaa08539a49
Raymon del2 <wambuiraymond03@gmail.com>
2026-03-18 22:43:12 +0300

    Fix Google account selection popup

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -21,5 +21,8 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+    prompt: 'select_account'
+});
 
 export default app;
