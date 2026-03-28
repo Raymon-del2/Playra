f1278b69c5c445ee1c7f5de795f8bcebea9730ac
Raymon del2 <wambuiraymond03@gmail.com>
2026-03-28 22:23:01 +0300

    Add comments to text posts on desktop too

--- src/components/CommunityPostCard.tsx
+++ src/components/CommunityPostCard.tsx
@@ -329,7 +329,7 @@ export default function CommunityPostCard({ post, onVote, onQuizAnswer }: PostPr
 
       {/* Engagement Bar */}
       <div className="post-engagement">
-        {isTextPost && isMobile && (
+        {isTextPost && (
           <button className="engage-btn" onClick={() => setShowComments(!showComments)}>
             <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
@@ -344,8 +344,8 @@ export default function CommunityPostCard({ post, onVote, onQuizAnswer }: PostPr
         </button>
       </div>
 
-      {/* Comments Dropdown for Text Posts on Mobile */}
-      {isTextPost && isMobile && showComments && (
+      {/* Comments Dropdown for Text Posts */}
+      {isTextPost && showComments && (
         <div className="post-comments-dropdown">
           <div className="comments-input-row">
             <input
